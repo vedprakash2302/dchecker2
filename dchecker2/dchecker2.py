f8bf52f471aac65f9a09c08ce3b791d5a4da871f
Vedprakash <vedprakash2302@gmail.com>
2019-09-11 16:17:30 -0400

    Read config file and print sections

--- dchecker2/dchecker2.py
+++ dchecker2/dchecker2.py
@@ -1,8 +1,12 @@
 import click
+import configparser
 
 
 @click.command()
 @click.argument("config_file")
 def main(config_file):
     """Dchecker2 is a tool to perform data integrity checks on VIVO's Researcher Database."""
-    print(config_file)
+    config_file = 'config/' + config_file
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    print(config.sections())
