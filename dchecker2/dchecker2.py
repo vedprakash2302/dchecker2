28802638c483d3fe9b26670527087418b392ba43
Vedprakash Pandey <vedprakash2302@gmail.com>
2019-09-16 09:20:20 -0400

    Add error handling while reading config file

--- dchecker2/dchecker2.py
+++ dchecker2/dchecker2.py
@@ -1,12 +1,26 @@
+import os
 import click
 import configparser
+from pathlib import Path
 
 
 @click.command()
 @click.argument("config_file")
 def main(config_file):
     """Dchecker2 is a tool to perform data integrity checks on VIVO's Researcher Database."""
+
+    # Echoes the text to the terminal using the specified color
+    def echo(text, color): return click.echo(click.style(text, color))
+
     config_file = 'config/' + config_file
+
     config = configparser.ConfigParser()
+
+    # Attempt to read config file. Show error if invalid file.
+    try:
+        config.read_file(open(config_file))
+    except:
+        echo("Configuration file path invalid.", "red")
+
     config.read(config_file)
-    print(config.sections())
+    echo(str(config.sections()), "green")
