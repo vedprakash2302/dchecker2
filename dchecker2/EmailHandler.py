f2fb3f9fdbf8c890c9200ccf9fe0a2adce06d3e8
Vedprakash Pandey <vedprakash2302@gmail.com>
2019-09-16 11:56:34 -0400

    Send email functionality

---
+++ dchecker2/EmailHandler.py
@@ -0,0 +1,27 @@
+import click
+import smtplib
+import configparser
+from datetime import datetime
+from email.mime.text import MIMEText
+from .utils import echo
+
+
+class EmailHandler:
+
+    def send(self, from_address, to_address, subject, body):
+        """Sends an email with specified body.
+        Keyword arguments:
+        subject -- subject of the report.
+        body -- the body as a string.
+        from_address -- email address the report will come from.
+        to_address -- email address the report is going to.
+        """
+
+        print(to_address)
+        msg = MIMEText(body)
+        msg['Subject'] = subject + "\n"
+        msg['From'] = from_address
+        msg['To'] = to_address
+        mail_connection = smtplib.SMTP("smtp.ufl.edu")
+        mail_connection.sendmail(from_address, to_address, msg.as_string())
+        mail_connection.quit()
--- dchecker2/dchecker2.py
+++ dchecker2/dchecker2.py
@@ -3,6 +3,7 @@ import click
 import configparser
 from pathlib import Path
 from .ReadConfig import ReadConfig
+from .EmailHandler import EmailHandler
 
 
 @click.command()
@@ -11,3 +12,6 @@ def main(config_file):
     """Dchecker2 is a tool to perform data integrity checks on VIVO's Researcher Database."""
 
     config = ReadConfig(config_file)
+    email = EmailHandler()
+    data_report = "Test"
+    email.send(config.from_address, config.to_address, config.subject, data_report)
