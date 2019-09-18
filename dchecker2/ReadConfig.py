b06b054f9be0c49227da446908c6f24d29cc0517
Vedprakash Pandey <vedprakash2302@gmail.com>
2019-09-18 09:32:26 -0400

    Restructure config parsing code

--- dchecker2/ReadConfig.py
+++ dchecker2/ReadConfig.py
@@ -1,6 +1,7 @@
 import os
 import click
 import configparser
+from datetime import datetime
 from .utils import echo
 
 
@@ -16,5 +17,29 @@ class ReadConfig:
             config.read_file(open(config_file))
         except:
             echo("Configuration file path invalid.", "red")
+
         config.read(config_file)
-        echo(str(config.sections()), "green")
+        self.read_arguments_from_config(config)
+
+    def read_arguments_from_config(self, config):
+        # Read email info
+        self.from_address = config.get('Email', 'from_address')
+        self.to_address = config.get('Email', 'to_address')
+        try:
+            self.subject = config.get('Email', 'subject')
+        except configparser.NoOptionError:
+            self.subject = "VIVO Data Quality report for"
+        self.subject = self.subject + " " + str(datetime.now())
+
+        # Read SPARQL endpoint
+        self.sparql_endpoint = config.get('SPARQL', 'endpoint')
+
+        # Read the directory which contains query files
+        try:
+            self.query_directory = config.get('Queries', 'folder')
+        except (configparser.NoSectionError, configparser.NoOptionError):
+            # portable for *NIX/Windows
+            if os.name == "nt":
+                self.query_directory = os.getcwd() + "\\queries\\"
+            elif os.name == "posix":
+                self.query_directory = os.getcwd() + "/queries/"
